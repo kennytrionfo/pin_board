4ac787c3c67be85a457b9edbf5f355cebd400437
Kenny T <ktrionfo@integracore.com>
2015-03-05 20:57:38 -0700

    last commit of night. playing with styles in application.css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306031205) do
+ActiveRecord::Schema.define(version: 20150306032705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20150306031205) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "restaurant_id"
   end
 
+  add_index "pins", ["restaurant_id"], name: "index_pins_on_restaurant_id", using: :btree
   add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
 
   create_table "restaurants", force: true do |t|
